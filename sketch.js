e92a69a259c2cee184b43fe62083e5a18d10e49f
Atinder-Pal <atty.pal46@gmail.com>
2020-12-21 11:13:25 -0700

    Added pickLocation method to show food on canvas

--- sketch.js
+++ sketch.js
@@ -1,16 +1,28 @@
 var snake;
 var scl =20;
+var food;
 
 function setup() {
   createCanvas(600, 600);
   snake = new Snake();
   frameRate(10);
+  pickLocation();
+}
+
+function pickLocation(){
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
 }
 
 function draw() {
   background(51);
   snake.update();
   snake.show();
+
+  fill(255, 0 , 100);
+  rect(food.x, food.y, scl, scl);
 }
 
 function keyPressed(){
