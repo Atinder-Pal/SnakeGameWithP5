b55032b8b01abf60917dc7842f67c564d610ddeb
Atinder-Pal <atty.pal46@gmail.com>
2020-12-21 11:36:25 -0700

    Added tail[] to store the length of snake as it increases on foodIntake;

--- snake.js
+++ snake.js
@@ -2,11 +2,14 @@ function Snake() {
     this.x = 0;
     this.y = 0;
     this.xspeed = 1;
-    this.yspeed = 0;     
+    this.yspeed = 0;   
+    this.total = 0;
+    this.tail =[];  
     
     this.eat = function(pos){
         let d = dist(this.x, this.y, pos.x, pos.y);
         if(d< 1){
+            total++;
             return true;
         } else {
             return false;
@@ -18,7 +21,12 @@ function Snake() {
       this.yspeed = y;
     };  
    
-    this.update = function() {     
+    this.update = function() {  
+      for(let i = 0; i< this.total-1; i++){
+          this.tail[i]= this.tail[i+1];
+      }
+      this.tail[total-1]= createVector(this.x, this,y);
+      
       this.x = this.x + this.xspeed * scl;
       this.y = this.y + this.yspeed * scl;
   
@@ -27,7 +35,11 @@ function Snake() {
     };
   
     this.show = function() {
-      fill(255);     
+      fill(255); 
+      for(let i = 0; i< this.total; i++) {
+        rect(this.tail[i].x, this.tail[i].y, scl, scl); 
+      }
+         
       rect(this.x, this.y, scl, scl);
     };
   }
