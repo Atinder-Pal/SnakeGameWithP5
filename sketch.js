74cef315dfee4b055afd5b7b9621a8f3bac0044f
Atinder-Pal <atty.pal46@gmail.com>
2020-12-22 12:39:41 -0700

    created basic update method in snake class;

--- sketch.js
+++ sketch.js
@@ -2,9 +2,10 @@ let snake;
 function setup(){
   createCanvas(600,600);
   snake = new Snake();
+  frameRate(10);
 }
 
-function draw(){
+function draw(){  
   background(220);
   snake.update();
   snake.show();
--- snake.js
+++ snake.js
@@ -2,13 +2,17 @@ class Snake{
   constructor(){
     this.body = [];
     this.body.push(createVector(0,0));
+    this.res = 10;
+    this.xdir = 1;
+    this.ydir = 0;
   }
   update(){
-
+    this.body[0].x += this.xdir * this.res;
+    this.body[0].y += this.ydir * this.res;;
   }
 
   show(){
     fill(0);
-    rect(this.body[0].x, this.body[0].y, 10, 10);
+    rect(this.body[0].x, this.body[0].y, this.res, this.res);
   }
 }
