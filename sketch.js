a422be2e4ad484bed50e8ed354ddbc3d70bc80ec
Atinder-Pal <atty.pal46@gmail.com>
2020-12-22 13:15:41 -0700

    Added eventListener for key pressed to manipulate snake movement

--- sketch.js
+++ sketch.js
@@ -11,3 +11,20 @@ function draw(){
   snake.show();
   
 }
+
+function keyPressed(){
+  switch(keyCode){
+    case UP_ARROW:
+      snake.setDir(0,-1);
+      break;
+    case DOWN_ARROW:
+      snake.setDir(0,1);
+      break;
+    case LEFT_ARROW:
+      snake.setDir(-1,0);
+      break;
+    case DOWN_ARROW:
+      snake.setDir(1,0);
+      break;
+  }
+}
--- snake.js
+++ snake.js
@@ -6,9 +6,26 @@ class Snake{
     this.xdir = 1;
     this.ydir = 0;
   }
+
+  setDir(x,y){
+    this.xdir = x;
+    this.ydir = y;
+  }
+
   update(){
+    
     this.body[0].x += this.xdir * this.res;
-    this.body[0].y += this.ydir * this.res;;
+    this.body[0].y += this.ydir * this.res;  
+    
+    if(this.body[0].x + this.res >= width){
+      this.body[0].x = 0;
+    }else if(this.body[0].y + this.res >= height){
+      this.body[0].y = 0;
+    } else if(this.body[0].x < 0){
+      this.body[0].x = width-this.res;
+    } else if(this.body[0].y  < 0){
+      this.body[0].y = height-this.res;
+    }
   }
 
   show(){
