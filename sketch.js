38f4877794d7af599a67646e246a1b03a78edf0f
Atinder-Pal <atty.pal46@gmail.com>
2020-12-22 13:47:03 -0700

    Displayed food on screen;

--- sketch.js
+++ sketch.js
@@ -1,15 +1,32 @@
 let snake;
+let food;
+let cols;
+let rows;
+
 function setup(){
-  createCanvas(600,600);
+  createCanvas(600,600);  
   snake = new Snake();
+  cols = floor(width/snake.res);
+  rows = floor(height/snake.res);
   frameRate(10);
+  foodLocation();
 }
 
-function draw(){  
+function foodLocation(){
+  let x = floor(random(cols)) * snake.res;
+  let y = floor(random(rows)) * snake.res;
+  food = createVector(x,y);
+}
+
+
+function draw(){ 
   background(220);
   snake.update();
-  snake.show();
+  snake.show(); 
   
+  noStroke();
+  fill(255,0,0);
+  rect(food.x,food.y,snake.res, snake.res);
 }
 
 function keyPressed(){
@@ -23,7 +40,7 @@ function keyPressed(){
     case LEFT_ARROW:
       snake.setDir(-1,0);
       break;
-    case DOWN_ARROW:
+    case RIGHT_ARROW:
       snake.setDir(1,0);
       break;
   }
--- snake.js
+++ snake.js
@@ -1,10 +1,10 @@
 class Snake{
   constructor(){
     this.body = [];
-    this.body.push(createVector(0,0));
-    this.res = 10;
+    this.body.push(createVector(0,0));    
     this.xdir = 1;
     this.ydir = 0;
+    this.res = 10;
   }
 
   setDir(x,y){
@@ -12,11 +12,9 @@ class Snake{
     this.ydir = y;
   }
 
-  update(){
-    
+  update(){ 
     this.body[0].x += this.xdir * this.res;
-    this.body[0].y += this.ydir * this.res;  
-    
+    this.body[0].y += this.ydir * this.res;     
     if(this.body[0].x + this.res >= width){
       this.body[0].x = 0;
     }else if(this.body[0].y + this.res >= height){
